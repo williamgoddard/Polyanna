27a2fab13430437d1be2e37636071eb5399b9fae
William Goddard <72050588+williamgoddard@users.noreply.github.com>
2022-09-24 21:42:07 +0100

    Added character and room commands

--- src/main/java/me/wgoddard/PolyannaBot.java
+++ src/main/java/me/wgoddard/PolyannaBot.java
@@ -6,7 +6,6 @@ import me.wgoddard.listeners.ReadyListener;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.interactions.commands.Command;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.*;
 import net.dv8tion.jda.api.requests.GatewayIntent;
@@ -46,8 +45,7 @@ public class PolyannaBot {
                 new OptionData(OptionType.STRING, "item", "The name of the item", true)
                         .setRequiredLength(1, 30),
                 new OptionData(OptionType.INTEGER, "num", "The number of the specific item, if there are multiple items with the same name", false)
-                        .setRequiredRange(1, Integer.MAX_VALUE)
-        ));
+                        .setRequiredRange(1, Integer.MAX_VALUE)));
 
         commands.add(Commands.slash("take", "Take an item from the room into your inventory").addOptions(
                 new OptionData(OptionType.STRING, "item", "The name of the item", true)
@@ -78,6 +76,34 @@ public class PolyannaBot {
                         .setRequiredLength(1, 30),
                 new OptionData(OptionType.INTEGER, "num", "The number of the specific item, if there are multiple items with the same name", false)
                         .setRequiredRange(1, Integer.MAX_VALUE)));
+
+        commands.add(Commands.slash("room", "Command to perform admin actions on rooms").addSubcommands(
+                new SubcommandData("create", "Create a room").addOptions(
+                        new OptionData(OptionType.STRING, "name", "The name of the room", true)
+                                .setRequiredLength(1, 30)),
+                new SubcommandData("look", "Look at a room").addOptions(
+                        new OptionData(OptionType.STRING, "name", "The name of the room", true)
+                                .setRequiredLength(1, 30)),
+                new SubcommandData("edit", "Edit a room").addOptions(
+                        new OptionData(OptionType.STRING, "name", "The name of the room", true)
+                                .setRequiredLength(1, 30)),
+                new SubcommandData("delete", "Delete a room").addOptions(
+                        new OptionData(OptionType.STRING, "name", "The name of the room", true)
+                                .setRequiredLength(1, 30))));
+
+        commands.add(Commands.slash("character", "Command to perform admin actions on characters").addSubcommands(
+                new SubcommandData("create", "Create a character").addOptions(
+                        new OptionData(OptionType.STRING, "name", "The name of the character", true)
+                                .setRequiredLength(1, 30)),
+                new SubcommandData("look", "Look at a character").addOptions(
+                        new OptionData(OptionType.STRING, "name", "The name of the character", true)
+                                .setRequiredLength(1, 30)),
+                new SubcommandData("edit", "Edit a character").addOptions(
+                        new OptionData(OptionType.STRING, "name", "The name of the character", true)
+                                .setRequiredLength(1, 30)),
+                new SubcommandData("delete", "Delete a character").addOptions(
+                        new OptionData(OptionType.STRING, "name", "The name of the character", true)
+                                .setRequiredLength(1, 30))));
 
         commands.add(Commands.slash("item", "Command to perform admin actions on items").addSubcommands(
                 new SubcommandData("create", "Create an item").addOptions(
@@ -141,10 +167,9 @@ public class PolyannaBot {
                         new OptionData(OptionType.STRING, "location", "The name of the location of the item", true)
                                 .setRequiredLength(1, 30),
                         new OptionData(OptionType.STRING, "name", "The name of the item", true)
-                                .setRequiredLength(1, 30))
-        ));
+                                .setRequiredLength(1, 30))));
 
-                bot.getGuildById("933432644857909339").updateCommands().addCommands(commands).queue();
+        bot.getGuildById("933432644857909339").updateCommands().addCommands(commands).queue();
 
     }
 
